6fc14b8233bd7989aedb49822f9fcabd2b183d6b
Akhlaken07 <148112697+Akhlaken07@users.noreply.github.com>
2024-06-10 22:37:56 +0800

    test update

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div style="display: flex; flex-direction: column; justify-content: flex-start; align-items: center; height: 100vh;">
+    <h1>Login</h1><br>
+
+    <form action="homepage.html" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username"><br><br>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password"><br><br>
+    <input type="submit" value="Login">
+    </form>
+    </div>
+
+</body>
+</html>
