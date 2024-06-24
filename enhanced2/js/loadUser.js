cc365c763bd917212d5ee427bf8f1899b92a3a28
Akhlaken07 <148112697+Akhlaken07@users.noreply.github.com>
2024-06-24 15:31:13 +0800

    test

--- enhanced2/js/loadUser.js
+++
@@ -1,4 +0,0 @@
-fetch('displayUser.php')
-                        .then(response => response.text())
-                        .then(html => document.getElementById('data-container').innerHTML = html)
-                        .catch(error => console.error('Error loading the data:', error));
